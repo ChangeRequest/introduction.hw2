ddea9714bf55d5ecb1cee4183735a89c19e913dc
Ihor Kandaurov <heroy44@gmail.com>
2016-11-22 10:52:15 +0200

    Task2 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task2.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task2.java
@@ -1,8 +1,18 @@
 package school.lemon.changerequest.java.introduction.hw2;
 
 
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task2 {
     public static String firstTwo(String s) {
-        return "";
+
+        if (s == null) return null;
+        else if (s.length() < 2) return s;
+        else {
+            return s.substring(0, 2);
+        }
     }
 }
