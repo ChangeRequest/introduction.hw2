bec21260dbe66ab9cf271e46b6fa026d9a0f127e
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2016-11-22 21:59:09 +0200

    Task 1 done.

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task1.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task1.java
@@ -1,7 +1,8 @@
 package school.lemon.changerequest.java.introduction.hw2;
 
 public class Task1 {
-    public static String makeTags(String tag, String text) {
-        return "";
+    public static String makeTags(String tag, String text)
+    {
+        return "<"+tag+">"+text+"</"+tag+">";
     }
 }
