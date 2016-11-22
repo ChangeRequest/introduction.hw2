757d25547e39eaca47521319a8a36130a157845e
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2016-11-22 22:53:24 +0200

    Task 3 done.

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task3.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task3.java
@@ -2,7 +2,17 @@ package school.lemon.changerequest.java.introduction.hw2;
 
 
 public class Task3 {
-    public static String comboString(String s1, String s2) {
-        return "";
+    public static String comboString(String s1, String s2)
+    {
+        if(s1 == null)
+            return s1+s2+s1;
+        else
+        if(s2 == null)
+            return s2 + s1 + s2;
+        else
+        if(s1.length() < s2.length())
+            return s1+s2+s1;
+
+                return s2+s1+s2;
     }
 }
