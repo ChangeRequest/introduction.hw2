010480493a87a9cf4cffbcb112004ece7e4a6e9b
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2016-11-23 00:00:06 +0200

    Task 7 done,

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task7.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task7.java
@@ -2,6 +2,14 @@ package school.lemon.changerequest.java.introduction.hw2;
 
 public class Task7 {
     public static int countEvens(int[] arr) {
-        return 0;
+        int count=0;
+        if(arr == null)
+            return 0;
+        for (int anArr : arr) {
+            if (anArr % 2 == 0)
+                count++;
+
+        }
+        return count;
     }
 }
