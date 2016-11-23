db13136db0e1ba814be8ba4d0d321be93c3d73b9
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2016-11-23 22:56:08 +0200

    Task 6 done,

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task6.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task6.java
@@ -3,6 +3,15 @@ package school.lemon.changerequest.java.introduction.hw2;
 
 public class Task6 {
     public static int[] reverse(int[] arr) {
-        return null;
+        if (arr == null) {
+            return null;
+        }
+        int[]a = new int[arr.length];
+        for(int i = 0;i < arr.length;i++)
+        {
+            a[arr.length-1-i] = arr[i];
+
+        }
+        return a;
     }
 }
