1ea61eaedb74e7954747cad371ad0958c580a1ab
Ihor Kandaurov <heroy44@gmail.com>
2016-11-24 13:44:44 +0200

    Task 9 not done

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task9.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task9.java
@@ -1,7 +1,21 @@
 package school.lemon.changerequest.java.introduction.hw2;
 
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task9 {
     public static String[] extractData(String URL) {
+        if (URL == null) {
+            return new String[]{"", "", ""};
+        }
+//        Pattern port = Pattern.compile(".*:(\\d+).*");
+//        Matcher portm = port.matcher(url);
+//        Pattern scheme = Pattern.compile(":\\\\([\\w\\-.]+)|(www\\.\\w+.\\w+)\n");
+//        Matcher schemem = scheme.matcher(url);
+//        Pattern host = Pattern.compile("([a-zA-Z]+)://");
+//        Matcher hostm = host.matcher(url);
+
         return new String[]{"", "", ""};
     }
 }
