5fa297357d973928ca35d51b91dae5b69b3db78b
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-24 16:29:12 +0200

    2 not all (i have some trouble with null)

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task2.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task2.java
@@ -3,6 +3,22 @@ package school.lemon.changerequest.java.introduction.hw2;
 
 public class Task2 {
     public static String firstTwo(String s) {
-        return "";
+
+
+        StringBuilder shortstr = new StringBuilder(s);
+
+
+        if (s.length() == 0) {
+            return s;
+        } else if (s.length() == 1) {
+            return s;
+        } else if (s == null) {
+            return null;
+        } else {
+            shortstr.setLength(2);
+            String substring = shortstr.substring(0);
+            return substring;
+        }
+
     }
 }
