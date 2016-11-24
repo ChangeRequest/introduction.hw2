f54f75819228af3dae85af54cbd33f3e7bf13c4d
Ihor Kandaurov <heroy44@gmail.com>
2016-11-25 01:18:24 +0200

    task 9 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task9.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task9.java
@@ -1,7 +1,5 @@
 package school.lemon.changerequest.java.introduction.hw2;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,15 +8,15 @@ public class Task9 {
         if (URL == null) {
             return new String[]{"", "", ""};
         }
-        Pattern p = Pattern.compile("(\\w+)://(\\w+\\-?\\w+\\.?\\w+):?(\\d+)?");
-        List<String> list = new ArrayList();
+        Pattern p = Pattern.compile("(?:([a-z]+)://)?([\\da-z._-]+)(?::(\\d+)?)?.*");
         Matcher m = p.matcher(URL);
-        while (m.find()) {
-            list.add(m.group(1) + ", " + m.group(2) + ", " + m.group(3));
-            list.toString();
-            return new String[]{String.valueOf(list)};
-        }
 
+        if (m.matches()) {
+            String schema = m.group(1);
+            String host = m.group(2);
+            String port = m.group(3);
+            return new String[]{schema != null ? schema : "", host != null ? host : "", port != null ? port : ""};
+        }
         return new String[]{"", "", ""};
     }
-}//        , ":\\\\([\\w\\-.]+)|(www\\.\\w+.\\w+)\n", ""
+}
