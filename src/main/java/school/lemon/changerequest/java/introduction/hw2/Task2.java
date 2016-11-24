59786c3a3e6f68026ea70f06b6df0dc34e6be61d
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-24 21:15:14 +0200

    2 done changed

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task2.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task2.java
@@ -4,17 +4,14 @@ package school.lemon.changerequest.java.introduction.hw2;
 public class Task2 {
     public static String firstTwo(String s) {
 
-
-        StringBuilder shortstr = new StringBuilder(s);
-
-
-        if (s.length() == 0) {
-            return s;
-        } else if (s.length() == 1) {
-            return s;
-        } else if (s == null) {
+        if (s == null)
             return null;
-        } else {
+        StringBuilder shortstr = new StringBuilder(s);
+        if (s.length() < 2)
+            return s;
+
+
+        else {
             shortstr.setLength(2);
             String substring = shortstr.substring(0);
             return substring;
