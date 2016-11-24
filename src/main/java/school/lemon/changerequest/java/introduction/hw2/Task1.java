3a2d2c4338604911a280ee773c2fce4c8f3819db
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-24 15:25:54 +0200

    1 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task1.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task1.java
@@ -2,6 +2,10 @@ package school.lemon.changerequest.java.introduction.hw2;
 
 public class Task1 {
     public static String makeTags(String tag, String text) {
-        return "";
+        String opentag = "<" + tag + ">";
+        String closetag = "</" + tag + ">";
+        String html = opentag + text + closetag;
+
+        return html;
     }
 }
