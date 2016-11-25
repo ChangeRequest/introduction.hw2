0e6450d1742314048c649b00d2fe26195c4a1599
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2016-11-26 01:20:26 +0200

    Fixed Task 1

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task1.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task1.java
@@ -1,10 +1,8 @@
 package school.lemon.changerequest.java.introduction.hw2;
 
-import static java.lang.String.format;
-
 public class Task1 {
     public static String makeTags(String tag, String text)
     {
-        return format("%1$s%2$s%3$s%4$s%5$s%2$s%3$s", "<", tag, ">", text, "</", tag, ">");
+        return String.format("%1$s%2$s%3$s%4$s%5$s%2$s%3$s", "<", tag, ">", text, "</", tag, ">");
     }
 }
