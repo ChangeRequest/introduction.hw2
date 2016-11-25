6cac7fce03467948a827e1be8025214e1323158c
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-25 22:15:21 +0200

    2 done ( check string for null and for length<2 together )

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task2.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task2.java
@@ -4,18 +4,12 @@ package school.lemon.changerequest.java.introduction.hw2;
 public class Task2 {
     public static String firstTwo(String s) {
 
-        if (s == null)
-            return null;
-        StringBuilder shortstr = new StringBuilder(s);
-        if (s.length() < 2)
+
+        if (s == null || s.length() < 2)
             return s;
 
+        return s.substring(0, 2);
 
-        else {
-            shortstr.setLength(2);
-            String substring = shortstr.substring(0);
-            return substring;
-        }
 
     }
 }
