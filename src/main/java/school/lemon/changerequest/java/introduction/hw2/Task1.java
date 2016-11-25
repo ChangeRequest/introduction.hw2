e1d526dc90e6ddd12753d513a06d8ac8c3eec07e
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-25 21:46:29 +0200

    1 done ( with String.format() )

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task1.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task1.java
@@ -1,11 +1,13 @@
 package school.lemon.changerequest.java.introduction.hw2;
 
+import java.util.Formatter;
+
 public class Task1 {
     public static String makeTags(String tag, String text) {
-        String opentag = "<" + tag + ">";
-        String closetag = "</" + tag + ">";
-        String html = opentag + text + closetag;
 
-        return html;
+
+        return String.format("%s", "<" + tag + ">"
+                + text + "</" + tag + ">");
+
     }
 }
