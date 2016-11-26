ca9406c97a67caefa678a53d48cdffd02daf0353
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-26 21:58:23 +0200

    5 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task5.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task5.java
@@ -2,6 +2,12 @@ package school.lemon.changerequest.java.introduction.hw2;
 
 public class Task5 {
     public static boolean commondEnd(int[] a, int[] b) {
+        int lengthA = a.length;
+        int lengthB = b.length;
+
+        if (lengthA >= 1 && lengthB >= 1) {
+            return a[0] == b[0] || a[a.length - 1] == b[b.length - 1] || a[0] == b[b.length - 1] || a[a.length - 1] == b[0];
+        }
         return false;
     }
 }
