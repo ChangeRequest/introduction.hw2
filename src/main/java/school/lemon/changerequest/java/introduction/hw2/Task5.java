76c2630540bc15ef476534d9623617d38ef1e40e
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-27 21:34:07 +0200

    5 done (changed without if )

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task5.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task5.java
@@ -4,10 +4,7 @@ public class Task5 {
     public static boolean commondEnd(int[] a, int[] b) {
 
 
-        if (a[0] == b[0] || a[0] == b[b.length - 1] || a[a.length - 1] == b[0])
-            return true;
-
-        return false;
+        return (a[0] == b[0] || a[0] == b[b.length - 1] || a[a.length - 1] == b[0] || a[a.length - 1] == b[b.length - 1]);
 
 
     }
