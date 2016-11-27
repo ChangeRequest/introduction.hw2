c70745ee268ae15cc9b112101470a8ca1e0ce52e
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-28 00:22:18 +0200

    7 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task7.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task7.java
@@ -2,6 +2,14 @@ package school.lemon.changerequest.java.introduction.hw2;
 
 public class Task7 {
     public static int countEvens(int[] arr) {
-        return 0;
+        if (arr == null) {
+            return 0;
+        }
+        int n = 0;
+        for (int i = arr.length - 1; i >= 0; --i)
+            if (arr[i] % 2 == 0 || arr[i] == 0) {
+                n++;
+            }
+        return n;
     }
 }
