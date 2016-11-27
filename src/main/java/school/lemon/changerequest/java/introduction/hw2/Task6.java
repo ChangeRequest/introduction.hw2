f18903053597d46c57b287134327d9d180be1c79
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-11-28 00:05:08 +0200

    6 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task6.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task6.java
@@ -1,8 +1,19 @@
 package school.lemon.changerequest.java.introduction.hw2;
 
 
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
 public class Task6 {
     public static int[] reverse(int[] arr) {
-        return null;
+        if (arr == null) {
+            return null;
+        }
+        int reverseArr[] = new int[arr.length];
+        int n = 0;
+        for (int i = arr.length - 1; i >= 0; --i) {
+            reverseArr[i] = arr[n];
+            n++;
+        }
+        return reverseArr;
     }
 }
