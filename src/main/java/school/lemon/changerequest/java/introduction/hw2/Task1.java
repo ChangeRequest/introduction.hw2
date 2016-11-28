034895e004e822943a6feb20b1750b13f776d9df
Olga Berezhna <berezhna.o.a@gmail.com>
2016-11-28 14:26:59 +0200

    t1_done

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task1.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task1.java
@@ -2,6 +2,7 @@ package school.lemon.changerequest.java.introduction.hw2;
 
 public class Task1 {
     public static String makeTags(String tag, String text) {
-        return "";
+
+        return String.format("%s", "<" + tag + ">" + text + "</" + tag + ">");
     }
 }
