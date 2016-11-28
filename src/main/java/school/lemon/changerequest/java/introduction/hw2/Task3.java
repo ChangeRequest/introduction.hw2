bdef191d359bea7a6c0319b92e4c774fc4e180cb
Igor Afanasenko <igoraf07@gmail.com>
2016-11-28 17:59:35 +0200

    Task 3 fixed.

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task3.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task3.java
@@ -4,10 +4,10 @@ package school.lemon.changerequest.java.introduction.hw2;
 public class Task3 {
     public static String comboString(String s1, String s2) {
         if (s1 == null) {
-            s1 = String.valueOf(s1);
-        }
+            return String.valueOf(s1)+s2+String.valueOf(s1);
+                    }
         if (s2 == null) {
-            s2 = String.valueOf(s2);
+            return String.valueOf(s2)+s1+String.valueOf(s2);
         }
         int length1 = s1.length();
         int length2 = s2.length();
