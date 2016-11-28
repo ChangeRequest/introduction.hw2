98003138f89feb9364ac44988f940b400736de5b
Olga Berezhna <berezhna.o.a@gmail.com>
2016-11-28 17:35:32 +0200

    t3_done

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task3.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task3.java
@@ -3,6 +3,20 @@ package school.lemon.changerequest.java.introduction.hw2;
 
 public class Task3 {
     public static String comboString(String s1, String s2) {
-        return "";
+        if (s1 == null){
+            s1 = String.valueOf(s1);
+        }
+
+        if (s1.length() > s2.length())
+        {
+            StringBuffer newstring = new StringBuffer();
+            newstring.append(s2).append(s1).append(s2);
+            return newstring.toString();
+        }
+        else {
+            StringBuffer newstring2 = new StringBuffer();
+            newstring2.append(s1).append(s2).append(s1);
+            return newstring2.toString();
+        }
     }
 }
