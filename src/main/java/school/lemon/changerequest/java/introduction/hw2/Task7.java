19101d1327c83be7c328754cb74827b31e1ba950
Olga Berezhna <berezhna.o.a@gmail.com>
2016-11-30 20:43:18 +0200

    t8_done

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task7.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task7.java
@@ -4,7 +4,9 @@ public class Task7 {
     public static int countEvens(int[] arr) {
         int evenNum = 0;
         for (int anArr : arr) {
-            if (anArr % 2 == 0) evenNum++;
+            if (anArr % 2 == 0) {
+                evenNum++;
+            }
         }
 
         return evenNum;
--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task8.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task8.java
@@ -3,6 +3,21 @@ package school.lemon.changerequest.java.introduction.hw2;
 public class Task8 {
 
     public static int[] evenOdd(int[] arr) {
-        return null;
+
+        int sortArr[] = new int[arr.length];
+        int j = 0;
+        for (int anArr : arr) {
+            if (anArr % 2 == 0)
+                sortArr[j++] = arr[anArr];
+        }
+
+        int n = 0;
+        for (int newArr: arr) {
+            if (newArr % 2 != 0) {
+                sortArr[j + n++] = arr[newArr];
+            }
+        }
+
+        return sortArr;
     }
 }
