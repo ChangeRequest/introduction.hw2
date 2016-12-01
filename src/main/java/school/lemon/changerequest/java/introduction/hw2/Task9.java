c09e6879781bba001c17c19bb35456966afc38ef
Olga Berezhna <berezhna.o.a@gmail.com>
2016-12-01 18:22:15 +0200

    t10_done

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task9.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task9.java
@@ -2,6 +2,7 @@ package school.lemon.changerequest.java.introduction.hw2;
 
 public class Task9 {
     public static String[] extractData(String URL) {
+
         return new String[]{"", "", ""};
     }
 }
