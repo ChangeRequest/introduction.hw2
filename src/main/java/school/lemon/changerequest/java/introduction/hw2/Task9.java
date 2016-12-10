205bcbec1b043d5065cdaa0a3c43a0618a051523
Olga Berezhna <berezhna.o.a@gmail.com>
2016-12-10 21:58:34 +0200

    Task 9 done

--- src/main/java/school/lemon/changerequest/java/introduction/hw2/Task9.java
+++ src/main/java/school/lemon/changerequest/java/introduction/hw2/Task9.java
@@ -1,8 +1,27 @@
 package school.lemon.changerequest.java.introduction.hw2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task9 {
     public static String[] extractData(String URL) {
 
-        return new String[]{"", "", ""};
+        if (URL == null) {
+
+            return new String[]{"", "", ""};
+        }
+        Pattern pattern = Pattern.compile("(?:([a-z]+):\\/\\/)?([\\da-z._-]+)(?::(\\d+)?)?.*");
+        //"(?:(\w+):\/\/)?([\d\w._-]+)(?::(\d+)?)?.*"
+        Matcher matcher = pattern.matcher(URL);
+
+
+        if (matcher.matches()) {
+            String scheme = matcher.group(1);
+            String host = matcher.group(2);
+            String port = matcher.group(3);
+            return new String[] {scheme != null ? scheme : "", host != null ? host : "", port != null ? port : ""};
+
+        }
+        return new String[] {"", "", ""};
     }
 }
